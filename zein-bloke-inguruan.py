460758fef4d5aa7a196134f2aff53102f5b49494
bekereke <aaretxaga@barrutialde.com>
2017-11-21 12:26:51 +0100

    Stevek behean eta goian dituen blokeak adierazita

--- zein-bloke-inguruan.py
+++ zein-bloke-inguruan.py
@@ -1,1 +1,13 @@
- 
+from mcpi.minecraft import Minecraft
+from time import sleep
+mc = Minecraft.create()
+
+while True:
+    x,y,z = mc.player.getPos()
+    behe = mc.getBlock(x,y-1,z)
+    #Stevek bi bloke luzeko altuera du
+    goi = mc.getBlock(x,y+2,z)
+    print("goi " + str(goi))
+    print("behe " + str(ezker))
+    print("-------------")
+    sleep(2)
