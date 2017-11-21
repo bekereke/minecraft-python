b8e3cbe7e7dd4edb753859a271c5329fe30ab25a
bekereke <aaretxaga@barrutialde.com>
2017-11-21 12:41:54 +0100

    beheko blokearen aldagaia ezabatuta EZUSTEAN

--- zein-bloke-inguruan.py
+++ zein-bloke-inguruan.py
@@ -3,11 +3,19 @@ from time import sleep
 mc = Minecraft.create()
 
 while True:
-    x,y,z = mc.player.getPos()
-    behe = mc.getBlock(x,y-1,z)
+x,y,z = mc.player.getPos()
     #Stevek bi bloke luzeko altuera du
     goi = mc.getBlock(x,y+2,z)
+    aurre = mc.getBlock(x+1,y-1,z)
+    atze = mc.getBlock(x-1,y-1,z)
+    ezker = mc.getBlock(x,y-1,z-1)
+    eskuin = mc.getBlock(x,y-1,z+1)
+    print("aurre " + str(aurre))
+    print("atze " + str(atze))
     print("goi " + str(goi))
-    print("behe " + str(ezker))
+    print("behe " + str(behe))
+    print("ezker " + str(ezker))
+    print("eskuin " + str(eskuin))
     print("-------------")
     sleep(2)
+
